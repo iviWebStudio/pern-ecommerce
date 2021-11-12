c4f91bfcfd74528281755b61d4d0b1d53432902c
MartinMkrtchyan <mmartin1@rambler.ru>
2021-11-12 23:52:57 +0400

    update packages and index.js
    * pino helper for logging
    * refactoring index, added logdata

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 require("dotenv").config({path: __dirname + "/.env"});
 const http = require("http");
 const app = require("./app");
+const {logger} = require("./helpers/logger");
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 8080;
+const port = parseInt(process.env.PORT) || 8080;
 
-server.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`)
-});
+server.listen(port, () => logger.info(`App listening at http://localhost:${port}`));
