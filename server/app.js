17ee4981aeff96f3647ce1f6fb54a0e1322fb604
MartinMkrtchyan <mmartin1@rambler.ru>
2021-11-22 22:53:11 +0400

    added helmet package for secure headers

--- server/app.js
+++ server/app.js
@@ -6,12 +6,14 @@ const compression = require("compression");
 const routes = require("./routes");
 const {handleError} = require("./helpers/error");
 const invalidEndpoint = require("./middleware/invalidEndpoint");
+const helmet = require("helmet");
 
 const app = express();
 
 app.set("trust proxy", 1);
 app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
+app.use(helmet());
 app.use(compression());
 app.use(cookieParser());
 app.use("/api", routes);
